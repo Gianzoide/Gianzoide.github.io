bd2949e7156b59da3caca69c187060ec3bd6fabe
Gianzoide <giancarloderozza@gmail.com>
2025-02-14 23:16:24 -0300

    jogo da velha

---
+++ projects/jogo da velha/scripcheckEventsInSequence.js
@@ -0,0 +1,20 @@
+function checkEventsInSequence(events){
+
+    for (const element of events) {
+        switch (element[0]) {
+            case 3:
+                detonatorEvent();                
+                
+        
+            default:
+                console.log("error");
+        }
+
+    }
+
+}
+
+
+function detonatorEvent(){
+
+}
